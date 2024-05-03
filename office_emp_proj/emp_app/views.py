d7c1966dd6dbf377080dee296d54f8bd1eceaddd
Akash kapadi <kapadiakash1@gmail.com>
2024-05-03 17:31:50 +0530

    update

--- office_emp_proj/emp_app/views.py
+++ office_emp_proj/emp_app/views.py
@@ -1,19 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from .models import Employee,Role,Department
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def all_emp(request):
-    
-    return render(request, 'all_emp.html')
+    emps = Employee.objects.all()
+    context = {
+        'emps':emps
+    }
+    print(context)
+    return render(request, 'all_emp.html',context)
 
 def add_emp(request):
-    return render(request, 'add_emp.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept = int(request.POST['dept'])
+        role = int(request.POST['role'])
+        new_emp = Employee(first_name=first_name,last_name=last_name,salary=salary,bonus=bonus,dept_id=dept,phone=phone,role_id=role,hire_date=datetime.now())
+        new_emp.save()
+        return HttpResponse('employee added Successfully')
+    elif request.method=='GET':
+        return render (request,'add_emp.html')
+    else:
+        return HttpResponse("An Exception Occured! Employee Has Not Been Added")
+        
+    
 
-def remove_emp(request):
-    return render(request, 'remove_emp.html')
+def remove_emp(request , emp_id=0):
+    if emp_id:
+        try:
+            emp_to_be_removed = Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse('Employee Removed Successfully')
+        except:
+            return HttpResponse("Please Enter A Valid EMP ID")
+    emps = Employee.objects.all()
+    context = {
+        'emps': emps
+    }
+    return render(request, 'remove_emp.html', context)
 
 def filter_emp(request):
     return render(request, 'filter_emp.html')
